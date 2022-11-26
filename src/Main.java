10e415db3e6c867b4018f5ae0e7cc0ab994ee2c2
Stanislaw <stas.yarets234@gmail.com>
2022-11-26 14:15:38 +0200

    First commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            double d = 1.0/3;
+            int[] myNumbers = {1, 2, 3};
+            System.out.println(myNumbers[10]);
+            System.out.println("The 'try catch' is finished.");
+        } catch (Exception e) {
+            System.out.println("Something went wrong.");
+        }
+        System.out.println("Яой");
+    }
+}
